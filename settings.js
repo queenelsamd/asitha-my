091a79f93f3144200f243c30a2a587ad32f2bf2f
queenelsamd <mywhatsappbot79@gmail.com>
2025-02-28 14:08:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=HuIVnRqZ#9qrDNKz5C8qQfhzA6NDrq_on98GD9A_ggRT-_03dMQ4' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=jqZ3EKbC#sOPXn-rYmQFrQvuTwse8bO_NjWerimzJ1pBnvCVdlC8' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '9473737373,9363636367' : process.env.SUDO,
